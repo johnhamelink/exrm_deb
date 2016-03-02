895cbc46f8e0d064e1fd5aba4ed7c8292c40f443
John Hamelink <john@johnhamelink.com>
2016-03-02 12:01:35 +0000

    Make version numbers for dependencies more generic

--- mix.exs
+++ mix.exs
@@ -34,8 +34,8 @@ defmodule ExrmDeb.Mixfile do
   defp deps(_) do
     [
      {:exrm, "~> 1.0"},
-     {:timex, "~> 1.0.1"},
-     {:vex, "~> 0.5.5"}
+     {:timex, "~> 1.0"},
+     {:vex, "~> 0.5"}
     ]
   end
 
