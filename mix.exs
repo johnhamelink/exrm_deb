684b00cfacaabe568c2055a25769532a7e639b83
John Hamelink <john@johnhamelink.com>
2017-01-07 15:33:56 +0000

    Bump version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExrmDeb.Mixfile do
 
   def project do
     [app: :exrm_deb,
-     version: "0.0.7",
+     version: "0.1.0",
      elixir: "~> 1.3",
      description: "Create a deb for your elixir release with ease",
      build_embedded: Mix.env == :prod,
